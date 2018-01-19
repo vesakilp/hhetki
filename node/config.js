21599226a0c75184d260fb67f3601e4f01d549a8
Vesa Kilpiainen <vesakilp@gmail.com>
2018-01-19 09:21:35 +0200

    new configurations

--- node/config.js
+++ node/config.js
@@ -1,5 +1,5 @@
 var config = {};
-config.requestUrl = "https://api.telegram.org/bot162620888:AAEVQ8nB9mFbZmqdJW7FDtLiz1Td83FeYGY/sendMessage";
-config.dday = "2016-10-21 17:00:00";
-config.chatId = "-6480162";
+config.requestUrl = "https://api.telegram.org/bot162620888:AAHWWdgWXKZdAmomhpotPuv6facWiEoVZRE/sendMessage";
+config.dday = "2018-03-16 17:00:00";
+config.chatId = "-1001119907460";
 module.exports = config;
